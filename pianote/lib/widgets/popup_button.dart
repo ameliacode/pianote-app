a5e20ca21cbf77060cdf69e913e144d71ed37c8f
ameliacode <ameliacode@hanyang.ac.kr>
2024-06-02 18:11:21 +0900

    update drawer tab

---
+++ pianote/lib/widgets/popup_button.dart
@@ -0,0 +1,156 @@
+import 'package:custom_pop_up_menu/custom_pop_up_menu.dart';
+import 'package:flutter/material.dart';
+import 'package:getwidget/getwidget.dart';
+import 'package:unicons/unicons.dart';
+
+class ItemModel {
+  String title;
+  IconData icon;
+  ItemModel(this.title, this.icon);
+}
+
+class PopupButton extends StatefulWidget {
+  final Container button;
+  final CustomPopupMenuController menuController;
+  final List<ItemModel> menuItems;
+  const PopupButton({Key? key, required this.button, required this.menuController, required this.menuItems}) : super(key: key);
+  @override
+ _PopupButtonState createState() => _PopupButtonState();
+}
+
+class _PopupButtonState extends State<PopupButton> {
+  @override
+  Widget build(BuildContext context) {
+    return CustomPopupMenu(
+      arrowColor: Colors.white,
+      child: widget.button,
+      menuBuilder: () => ClipRRect(
+        borderRadius: BorderRadius.circular(5),
+        child: Container(
+          color: Colors.white,
+          child: IntrinsicWidth(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: 
+                widget.menuItems.map(
+                (item) => GestureDetector(
+                  behavior: HitTestBehavior.translucent,
+                  onTap: () {
+                    print("onTap");
+                    widget.menuController.hideMenu();
+                  },
+                  child: Container(
+                    height: 40,
+                    padding: EdgeInsets.symmetric(horizontal: 10),
+                    child: Row(
+                      children: <Widget>[
+                        Icon(
+                          item.icon,
+                          size: 15,
+                          color: Colors.grey,
+                        ),
+                        Expanded(
+                                child: Container(
+                          margin: EdgeInsets.only(left: 10),
+                          padding:
+                              EdgeInsets.symmetric(vertical: 10),
+                          child: Text(
+                            item.title,
+                            style: TextStyle(
+                              color: Colors.grey,
+                              fontSize: 12,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            )
+            .toList(),
+          ),
+        ),
+      ),
+    ),
+    pressType: PressType.singleClick,
+    verticalMargin: -10,
+    controller: widget.menuController,
+    );
+  }
+}
+
+// buttons for each in drawer tab
+
+// filter button for sorting and viewing(later)
+
+class EditPopupButton extends StatefulWidget {
+  final CustomPopupMenuController menuController;
+  const EditPopupButton({Key? key, required this.menuController,}) : super(key: key);
+  @override
+ _EditPopupButtonState createState() => _EditPopupButtonState();
+}
+
+class _EditPopupButtonState extends State<EditPopupButton> {
+  late List<ItemModel> menuItems;
+
+  @override
+  void initState() {
+    super.initState();
+    menuItems = [
+      ItemModel('수정', UniconsLine.edit_alt),
+      ItemModel('삭제', UniconsLine.trash)
+    ];
+  }
+  
+  @override
+  Widget build(BuildContext context) {
+    return PopupButton(
+      button:Container(
+        child: Text('선택', 
+        style: TextStyle(fontWeight: FontWeight.normal, fontSize: 11.0, color: Colors.blueAccent),),
+        color: Colors.white,
+        padding: EdgeInsets.all(4.0),
+      ),  
+      menuController: widget.menuController,
+      menuItems: menuItems
+    );
+  }
+}
+
+// editing button 
+class FilterPopupButton extends StatefulWidget {
+  final CustomPopupMenuController menuController;
+  const FilterPopupButton({Key? key, required this.menuController,}) : super(key: key);
+  @override
+  _FilterPopupButtonState createState() => _FilterPopupButtonState();
+}
+
+class _FilterPopupButtonState extends State<FilterPopupButton> {
+  late List<ItemModel> menuItems;
+
+  @override
+  void initState() {
+    super.initState();
+    menuItems = [
+      ItemModel('정렬', UniconsLine.sorting),
+    ];
+  }
+ 
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return PopupButton(
+      button: Container(
+        child:  Icon(
+          UniconsLine.ellipsis_h, color: Colors.blueAccent,
+          size: 15,
+        ), 
+        padding: EdgeInsets.all(5.0),
+        color: Colors.white,
+      ),
+      menuController: widget.menuController, 
+      menuItems: menuItems
+    );
+  }
+}
--- pianote/lib/widgets/sheet_drawer_files.dart
+++ pianote/lib/widgets/sheet_drawer_files.dart
@@ -2,10 +2,12 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:getwidget/getwidget.dart';
+import 'package:pianote/widgets/popup_button.dart';
 import 'package:pianote/widgets/sheet_drawer_file_list.dart';
 import 'package:unicons/unicons.dart';
 import 'package:pianote/providers/recent_file_provider.dart';
 import 'package:animated_search_bar/animated_search_bar.dart';
+import 'package:custom_pop_up_menu/custom_pop_up_menu.dart';
 
 class SheetDrawerFiles extends StatefulWidget {
   const SheetDrawerFiles({Key? key, required this.recentProvider}) : super(key: key);
@@ -16,18 +18,22 @@ class SheetDrawerFiles extends StatefulWidget {
 
 class _FileManagerState extends State<SheetDrawerFiles> {
   late String searchValue;
-  late TextEditingController controller;
+  late TextEditingController textController;
+  late CustomPopupMenuController editController;
+  late CustomPopupMenuController utilController;
 
   @override
   void initState() {
     super.initState();
     searchValue = '';
-    controller = TextEditingController();
+    textController = TextEditingController();
+    editController = CustomPopupMenuController();
+    utilController = CustomPopupMenuController();
   }
 
   @override
   void dispose() {
-    controller.dispose();
+    textController.dispose();
     super.dispose();
   }
 
@@ -50,7 +56,7 @@ class _FileManagerState extends State<SheetDrawerFiles> {
                     searchDecoration: InputDecoration(
                       prefixText:null,
                       filled: true,
-                      fillColor: Colors.grey [100],
+                      fillColor: Colors.grey[100],
                       focusColor: Colors.grey,
                       alignLabelWithHint: false,
                       border: InputBorder.none,
@@ -58,8 +64,8 @@ class _FileManagerState extends State<SheetDrawerFiles> {
                       contentPadding: EdgeInsets.all(5.0)
                     ),
                     height:30.0,
-                    closeIcon: Icon(UniconsLine.times, color: Colors.grey, size: 20.0),
-                    searchIcon: Icon(UniconsLine.search, color: Colors.blueAccent, size: 20.0),
+                    closeIcon: Icon(UniconsLine.times, color: Colors.grey, size: 17.5),
+                    searchIcon: Icon(UniconsLine.search, color: Colors.blueAccent, size: 17.5),
                     label: "악보 및 작곡가명을 입력하세요.",
                     labelStyle: TextStyle(fontSize: 12.0, color: Colors.grey, fontWeight: FontWeight.normal),
                     searchStyle: TextStyle(fontSize: 14.0, fontWeight: FontWeight.normal),
@@ -83,14 +89,10 @@ class _FileManagerState extends State<SheetDrawerFiles> {
                   ),
                 ),
                 SizedBox(width:5.0),
-                GFIconButton(
-                  iconSize: 15.0,
-                  size: GFSize.SMALL,
-                  padding: EdgeInsets.all(5.0),
-                  color: Colors.white,
-                  icon: Icon(UniconsLine.filter, color: Colors.blueAccent,), 
-                  onPressed: () => {},
-                ),
+                SizedBox(
+                  width: 32.0,
+                  child: FilterPopupButton(menuController: utilController,)
+                )
               ],
             ),
           ),
